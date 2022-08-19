d527a790d288c553c895e761cfa542bf08330b1a
Paweł Jastrzębski <pj@napcode.eu>
2022-08-19 12:53:20 +0200

    Add admin to ShyDB

--- shynet/shydb/admin.py
+++ shynet/shydb/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# from django_json_widget.widgets import JSONEditorWidget
+
+from .models import ShyDB
+
+
+@admin.register(ShyDB)
+class ShyDBAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ['-updated_at']
+    # formfield_overrides = {
+    # models.JSONField: {'widget': JSONEditorWidget},
+    # }
